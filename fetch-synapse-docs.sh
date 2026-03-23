d50aae66fa73c16a559d06c2d71d98f1e03e90f6
Charith Ellawala <charith@cerbos.dev>
2026-03-23 17:15:57 +0000

    Fix script
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- fetch-synapse-docs.sh
+++ fetch-synapse-docs.sh
@@ -2,5 +2,6 @@
 
 #set -euo pipefail
 
-mkdir -p _synapse
-wget -O - -o /dev/null 'https://download.cerbos.dev/synapse/docs.zip' | unzip -d _synapse
+mkdir -p _synapse/docs
+wget -O _synapse/docs.zip -o /dev/null 'https://download.cerbos.dev/synapse/docs.zip'
+unzip -d _synapse/docs _synapse/docs.zip
