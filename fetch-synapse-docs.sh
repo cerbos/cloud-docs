97a2631f6ab1e1bbd87d63215885d1f86ca7cd01
Charith Ellawala <charith@cerbos.dev>
2026-03-23 17:06:12 +0000

    Use sh
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- fetch-synapse-docs.sh
+++ fetch-synapse-docs.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -euo pipefail
+#set -euo pipefail
 
 mkdir -p _synapse
 wget -O - -o /dev/null 'https://download.cerbos.dev/synapse/docs.zip' | unzip -d _synapse
